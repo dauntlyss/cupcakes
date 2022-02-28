331382ec292e891614bdad5303a8094a4f33806d
Alyssa Harris <amh302918@gmail.com>
2022-02-28 17:32:08 -0500

    added route for list_cupcakes()

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
 """Flask app for Cupcakes"""
+from flask import Flask, request, render_template, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pet_shop_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SECRET_KEY"] = "girlslovebeyonce"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+
+@app.route('/api/cupcakes')
+def list_cupcakes():
+    all_cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+
+    return jsonify(all_cupcakes)
