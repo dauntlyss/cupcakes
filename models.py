17fb2fbbe3f1ba8489b8f87c2eb6765f852bb69f
Alyssa Harris <amh302918@gmail.com>
2022-02-28 14:25:58 -0500

    Created cupcake model

--- models.py
+++ models.py
@@ -1,1 +1,28 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+GENERIC_IMAGE = "https://tinyurl.com/demo-cupcake"
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    """Delicious Cupcake model."""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        """Return explaination of cupcake."""
+
+        return f"<Cupcake: id={self.id}, flavor={self.flavor}, rating={self.rating}, image={self.image}>"
+
