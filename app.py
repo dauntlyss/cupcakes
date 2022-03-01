b67a6010306b76c084d03d0aa45169dadd5902f7
Alyssa Harris <amh302918@gmail.com>
2022-03-01 13:23:58 -0500

    correct sqlalchemy call

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from models import db, connect_db, Cupcake
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pet_shop_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SECRET_KEY"] = "girlslovebeyonce"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
